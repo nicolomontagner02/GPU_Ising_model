8ca86eb5f2cbae8e75f9945da9d2ac4566e1c2da
nicolomontagner02 <nicolo.montagnerstudenti.unipd.it>
2026-01-19 16:10:03 +0100

    "Magnetisation and energy"

--- Single_CPU/single_cpu_2D_ising.c
+++ Single_CPU/single_cpu_2D_ising.c
@@ -3,24 +3,47 @@
 #include <time.h>
 #include <math.h>
 
+int DEBUG = 0;
+
 float energy_2D(int **lattice, int size_x, int size_y, float J, float h) {
     float energy = 0.0;
     for (int i = 0; i < size_x; i++) {
         for (int j = 0; j < size_y; j++) {
             int spin = lattice[i][j];
-            int right_neighbor = lattice[i][(j + 1) % size_y];
-            if (i == size_x-1){
-                right_neighbor = 0;
+            int right_neighbor = 0;
+            int bottom_neighbor = 0;
+            if (j != size_y-1){
+                right_neighbor = lattice[i][j+1];
             }
-            int bottom_neighbor = lattice[(i + 1) % size_x][j];
-            if (j == size_y-1){
-                bottom_neighbor = 0;
+            if (i != size_x-1){
+                bottom_neighbor = lattice[i+1][j];
+            }
+            if (DEBUG){
+                printf("Right neighbor spin of (%i,%i): %i\n",i,j, right_neighbor);
+                printf("Bottom neighbor spin of (%i,%i): %i\n",i,j, bottom_neighbor);
             }
             energy -= J * spin * (right_neighbor + bottom_neighbor);
             energy -= h * spin;
         }
     }
     return energy;
+}
+
+float magnetisation_2D(int **lattice, int size_x, int size_y){
+
+    int N = size_x*size_y;
+
+    int magnetisation = 0;
+
+    for (int i = 0; i < size_x; i++){
+        for (int j = 0; j < size_y; j++){
+            magnetisation += lattice[i][j];
+        }
+    }
+
+    float avg_magnetisation = magnetisation / N;
+
+    return avg_magnetisation;
 }
 
 int main(int argc, char *argv[]) {
@@ -49,34 +72,41 @@ int main(int argc, char *argv[]) {
     switch(c){
         // Switch for different initializations cases
         case 1:
-        // Initialize to all spins up (+1)
-        for (int i = 0; i < lattice_size_x; i++) {
-            for (int j = 0; j < lattice_size_y; j++) {
-                lattice[i][j] = 1;
+            // Initialize to all spins up (+1)
+            for (int i = 0; i < lattice_size_x; i++) {
+                for (int j = 0; j < lattice_size_y; j++) {
+                    lattice[i][j] = 1;
+                }
             }
-        }
+            break;
 
         case 2:
-        // Initialize to all spins down (-1)
-        for (int i = 0; i < lattice_size_x; i++) {
-            for (int j = 0; j < lattice_size_y; j++) {
-                lattice[i][j] = -1;
+            // Initialize to all spins down (-1)
+            for (int i = 0; i < lattice_size_x; i++) {
+                for (int j = 0; j < lattice_size_y; j++) {
+                    lattice[i][j] = -1;
+                }
             }
-        }
-    
-        case 3:
-        // Initialize to random spins (+1 or -1)
-        srand(42); // Seed for reproducibility
-        for (int i = 0; i < lattice_size_x; i++) {
-            for (int j = 0; j < lattice_size_y; j++) {
-                lattice[i][j] = (rand() % 2) * 2 - 1; // Randomly assign +1 or -1
-            }
+            break;
 
-        }
+        case 3:
+            // Initialize to random spins (+1 or -1)
+            srand(2); // Seed for reproducibility
+            for (int i = 0; i < lattice_size_x; i++) {
+                for (int j = 0; j < lattice_size_y; j++) {
+                    lattice[i][j] = (rand() % 2) * 2 - 1; // Randomly assign +1 or -1
+                }
+
+            }
+            break;
+
+        default:
+            printf("Wrong initialization case submitted!\n");
+            return 1;
+
     }
         
-
-    //print the lattice
+    //print the lattice (only small size)
     if (lattice_size_x <=4 && lattice_size_y <=4){
         printf("2D Ising Lattice:\n");
         for (int i = 0; i < lattice_size_x; i++) {
@@ -89,6 +119,9 @@ int main(int argc, char *argv[]) {
     // Calculate and print the energy
     float total_energy = energy_2D(lattice, lattice_size_x, lattice_size_y, J, h);
     printf("Total Energy of the 2D Ising Lattice: %f\n", total_energy);
+
+    float magnetisation = magnetisation_2D(lattice, lattice_size_x, lattice_size_y);
+    printf("Total Magnetisation of the 2D Ising Lattice: %f\n", magnetisation);
 
     return 0;
 

f5e14d992e27c237936ff416addac19f5879cea2
nicolomontagner02 <nicolo.montagnerstudenti.unipd.it>
2026-02-07 17:13:01 +0100

    Simulation

--- Run_simulations.py
+++ Run_simulations.py
@@ -60,7 +60,7 @@ J_values      = [1.0]
 h_values      = [0.5, 1.0, 2.0]
 T_values      = [0.5, 2.0, 10.0]
 init_types    = {
-    1: "all_up",
+    # 1: "all_up",
 #    2: "all_down",
     3: "random",
 }

5f561be1735eacf070308ec110b4ac24be43b210
Nicolò <nicolo.montagner@studenti.unipd.it>
2026-02-13 10:42:18 +0000

    magnetisation

--- Regimes_control.c
+++ Regimes_control.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
         h_span[i] = h_s + i * dh;
     }
 
-    for (int ty = 3; ty < 4; ty += 2)
+    for (int ty = 1; ty < 4; ty += 3)
     {
         int type = ty; // 1 for all spin up, 2 for all spin down, 3 for random initialization of the lattice
 
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 
                     if (count % 7 == 0)
                     {
-                        save = 1;
+                        save = 0;
                         count = 0;
                     }
 
